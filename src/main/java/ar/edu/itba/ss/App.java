938400450ea876fc5173516378b9a3a1434426ec
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2020-03-20 01:21:53 -0300

    print to file

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -1,11 +1,9 @@
 package ar.edu.itba.ss;
 
-
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
-
 import static ar.edu.itba.ss.CellIndex.*;
 import static ar.edu.itba.ss.CommandParser.*;
 import static ar.edu.itba.ss.FileParser.*;
@@ -25,20 +23,22 @@ public class App {
             System.out.println("Invalid file name...");
             System.exit(1);
         }
+        File file = new File("output.txt");
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("output.txt", "UTF-8");
+        } catch (Exception e) {
+            System.out.println("Couldn't write output to file...");
+            System.exit(1);
+        }
+
         M = (int)Math.floor(L/RC);
         System.out.println("Time: " + T);
         System.out.println("M: " + M);
         System.out.println("Speed of particles: " + V);
-//        File file = new File("output.txt");
-//        PrintWriter writer = null;
-//        try {
-//            writer = new PrintWriter("output.txt", "UTF-8");
-//        } catch (Exception e) {
-//            System.out.println("Couldn't write output to file...");
-//            System.exit(1);
-//        }
+
         populateCells();
-        outputToFile(0);
+        outputToFile(0, writer);
         for(int i = 1 ; i <= T ; i++) {
             // calculate neighbours
             cellIndexAlgorithm();
@@ -53,29 +53,21 @@ public class App {
                 p.setTheta(angleMap.get(p));
             }
             // store values in file
-            outputToFile(i);
+            outputToFile(i, writer);
             // recalculate new particles in cell
             populateCells();
         }
-
+        writer.close();
         final long endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime - startTime) + " ms");
     }
 
-    private static void outputToFile(int time) {
-        File file2 = new File("./files/outputs/" + time + ".txt");
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter(file2, "UTF-8");
-        } catch (Exception e) {
-            System.out.println("Couldn't write output to file...");
-            System.exit(1);
-        }
+    private static void outputToFile(int time, PrintWriter writer) {
         writer.println(N);
+        writer.println("time: " + time);
         for (Particle p : particles) {
-            writer.print("\n" + p.getId() + " " + p.getX() + " " + p.getY() + " " + Math.cos(p.getTheta()) * V + " " + Math.sin(p.getTheta()) * V);
+            writer.println(p.getId() + " " + p.getX() + " " + p.getY() + " " + Math.cos(p.getTheta()) * V + " " + Math.sin(p.getTheta()) * V);
         }
 
-        writer.close();
     }
 }
